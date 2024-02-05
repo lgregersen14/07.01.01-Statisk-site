7dc8bbc13ec840e6e1c80e89e3a21f6379215236
Lucas Gregersen <lugr0001@stud.kea.dk>
2024-02-05 14:27:10 +0100

    nye ting

--- productlist.js
+++ productlist.js
@@ -1,26 +1,26 @@
 fetch("https://kea-alt-del.dk/t7/api/products")
-.then(res=>res.json())
-.then(showProducts)
+  .then((res) => res.json())
+  .then(showProducts);
 
-
-function showProducts(products){
-//looper og kalder showProduct
-products.forEach(showProduct)
+function showProducts(products) {
+  //looper og kalder showProduct
+  products.forEach(showProduct);
 }
 
-function showProducts(product){
-    console.log(product);
-    //fange template
-    const template = document.querySelector("smallProductTemplate").textContent;
-    //lav en kopi
-    const copy = template.cloneNode(true);
-    //aendre indhold
-copy.querySelector("h3").textCoontent=product.productdisplayname;
-if(product.soldout){
+function showProduct(product) {
+  console.log("mit produkt er ", product);
+  //fange template
+  const template = document.querySelector("#smallProductTemplate").content;
+  //lav en kopi
+  const copy = template.cloneNode(true);
+  //Alt under her er forskellige informationer
+
+  //aendre indhold
+  copy.querySelector("h1").textContent = product.productdisplayname;
+  if (product.soldout) {
     //produktet er udsolgt
-    copy.querySelector("article").classList.add("soldOut")
+    copy.querySelector("article").classList.add("soldOut");
+  }
+  //appende
+  document.querySelector("main").appendChild(copy);
 }
-    //appende
-    document.querySelector("main").appendChild(copy)
-
-    }
